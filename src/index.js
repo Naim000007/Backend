c37c1496ee07ab0d6f6987ebe77d16506f226501
Naim <mdnaim01303202218@gmail.com>
2023-12-28 11:21:52 +0600

    mongoDB connected

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import mongoose from "mongoose"
-import { DB_NAME } from './constants';
-import express from "express";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './.env'
+})
+
+
+
+connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
+
+/*
 
 const app = express()
-
 (async ()=>{
     try{
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
@@ -19,3 +35,9 @@ const app = express()
         throw error
     }
 })()
+*/
+
+
+
+
+
